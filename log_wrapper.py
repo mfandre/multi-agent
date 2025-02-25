a69add607030f43928adb81fa164d2e9f88e73c6
Andre de Mattos Ferraz <fandre@gmail.com>
2025-02-25 06:41:16 -0300

    adding log

---
+++ log_wrapper.py
@@ -0,0 +1,31 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+FILE = False
+
+log_formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s"
+)
+
+# Setting log config
+my_handler = None
+if FILE:
+    LOGFILE = "log.txt"
+    my_handler = RotatingFileHandler(
+        LOGFILE,
+        mode="a",
+        maxBytes=5 * 1024 * 1024,
+        backupCount=2,
+        encoding=None,
+        delay=0,
+    )
+else:
+    my_handler = logging.StreamHandler()
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+
+app_log = logging.getLogger("root")
+app_log.setLevel(logging.INFO)
+
+app_log.addHandler(my_handler)
